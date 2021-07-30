07a62637f38fa6c8faee459c2fec6f20725fed59
cheneeheng <eeheng.chen@gmail.com>
2021-07-30 16:15:25 +0200

    Housekeeping
    - Moved unused code in expporter.

--- data/exporter/exporter.py
+++ data/exporter/exporter.py
@@ -92,11 +92,10 @@ def get_patient_list():
     """.format(UNIT_TYPES)
     df = pd.read_sql_query(query, conn)
     df.sort_values('patientunitstayid', ascending=True, inplace=True)
+    conn.close()
 
     print(f"Number of patient data entries:", df.shape[0])
     print('-'*80)
-
-    conn.close()
 
     patientunitstayid_list = df['patientunitstayid'].tolist()
     return patientunitstayid_list
@@ -116,8 +115,7 @@ def get_multiple_data_and_save(output_folder,
     for patientunitstayid in pbar:
 
         time.sleep(1)
-        pbar.set_description(
-            f"Processing {patientunitstayid}")
+        pbar.set_description(f"Processing {patientunitstayid}")
 
         subset_path = f"{EXPORTER_SUBSET_FOLDER}/{patientunitstayid}.json"
         with open(subset_path, 'r') as json_file:
@@ -152,63 +150,6 @@ def get_multiple_data_and_save(output_folder,
     conn.close()
 
 
-def get_multiple_data_and_save_with_cursor(output_folder,
-                                           patientunitstayid_list=[],
-                                           pid=0):
-    """
-    Iterates over `patientunitstayid` and saves entries from all tables
-    with the similar id into json.
-    """
-
-    query_schema, conn = connect_to_database()
-    cur = conn.cursor()
-
-    now = time.time()
-    pbar = tqdm(patientunitstayid_list, position=pid+1)
-    for patientunitstayid in pbar:
-
-        time.sleep(1)
-        pbar.set_description(
-            f"Processing {patientunitstayid}")
-
-        json_dict = {}
-
-        for table_name in TABLE_LIST:
-
-            query = query_schema + """
-            select *
-            from {}
-            where patientunitstayid = {}
-            """.format(table_name, patientunitstayid)
-            cur.execute(query)
-            rows = cur.fetchall()
-            label = [desc[0] for desc in cur.description]
-
-            if len(rows) == 0:
-                json_dict[table_name] = {}
-                for idx, label_i in enumerate(label):
-                    json_dict[table_name][label_i] = []
-
-            else:
-                assert len(label) == len(rows[0])
-                json_dict[table_name] = {}
-                for idx, label_i in enumerate(label):
-                    col = [r[idx] for r in rows]
-                    col = [float(c)
-                           if isinstance(c, Decimal) else c
-                           for c in col]
-                    json_dict[table_name][label_i] = col
-
-        json_path = f"{output_folder}/{patientunitstayid}.json"
-        with open(json_path, 'w') as json_file:
-            json.dump(json_dict, json_file)
-
-    print("Total Time:", time.time()-now)
-    print('-'*80)
-
-    conn.close()
-
-
 def parallel_processing(func,
                         output_folder,
                         patientunitstayid_list=[]):
@@ -216,12 +157,12 @@ def parallel_processing(func,
     def npi(x):
         return math.ceil(len(x) / NUM_PROCESSES)
 
-    a0 = output_folder
-    a1 = patientunitstayid_list
+    num_id_per_process = npi(patientunitstayid_list)
 
     argument_list = [
-        (a0,
-         a1[pid*npi(a1):(pid+1)*npi(a1)],
+        (output_folder,
+         patientunitstayid_list[pid*num_id_per_process:
+                                (pid+1)*num_id_per_process],
          pid)
         for pid in range(NUM_PROCESSES)
     ]
@@ -232,36 +173,6 @@ def parallel_processing(func,
     jobs = [pool.apply_async(func, args=arg) for arg in argument_list]
     pool.close()
     result_list = [job.get() for job in jobs]
-    # print("\n" * (len(argument_list) + 1))
-
-
-def get_single_chunked_dataframe(output_folder):
-    """NOT USED. """
-
-    query_schema, conn = connect_to_database()
-
-    query = query_schema + """
-    select *
-    from {}
-    """.format(output_folder.split('/')[-1])
-
-    df_chunk_iter = pd.read_sql_query(query, conn, chunksize=1)
-
-    for idx, df_chunk in enumerate(df_chunk_iter):
-
-        patientunitstayid = df_chunk['patientunitstayid'].tolist()[0]
-        csv_path = f"{output_folder}/{patientunitstayid}.csv"
-
-        if os.path.exists(csv_path):
-            df_chunk.to_csv(csv_path, mode='a', header=False)
-        else:
-            df_chunk.to_csv(csv_path)
-
-        if idx % 1000 == 0:
-            print(f"{datetime.datetime.now()} | \
-                  {len(os.listdir(output_folder))}")
-
-    conn.close()
 
 
 if __name__ == "__main__":
@@ -271,7 +182,7 @@ if __name__ == "__main__":
     # 0. patient data from the icu list.
     patientunitstayid_list = get_patient_list()
 
-    # X. data from the patient list.
+    # 1. data from the patient list.
     # get_multiple_data_and_save(output_folder, patientunitstayid_list, 0)
 
     for i in range(START, CHUNKS, 1):
---
+++ data/exporter/exporter_legacy.py
@@ -0,0 +1,88 @@
+'''
+
+def get_single_chunked_dataframe(output_folder):
+    """NOT USED. """
+
+    query_schema, conn = connect_to_database()
+
+    query = query_schema + """
+    select *
+    from {}
+    """.format(output_folder.split('/')[-1])
+
+    df_chunk_iter = pd.read_sql_query(query, conn, chunksize=1)
+
+    for idx, df_chunk in enumerate(df_chunk_iter):
+
+        patientunitstayid = df_chunk['patientunitstayid'].tolist()[0]
+        csv_path = f"{output_folder}/{patientunitstayid}.csv"
+
+        if os.path.exists(csv_path):
+            df_chunk.to_csv(csv_path, mode='a', header=False)
+        else:
+            df_chunk.to_csv(csv_path)
+
+        if idx % 1000 == 0:
+            print(f"{datetime.datetime.now()} | \
+                  {len(os.listdir(output_folder))}")
+
+    conn.close()
+
+
+def get_multiple_data_and_save_with_cursor(output_folder,
+                                           patientunitstayid_list=[],
+                                           pid=0):
+    """
+    Iterates over `patientunitstayid` and saves entries from all tables
+    with the similar id into json.
+    """
+
+    query_schema, conn = connect_to_database()
+    cur = conn.cursor()
+
+    now = time.time()
+    pbar = tqdm(patientunitstayid_list, position=pid+1)
+    for patientunitstayid in pbar:
+
+        time.sleep(1)
+        pbar.set_description(
+            f"Processing {patientunitstayid}")
+
+        json_dict = {}
+
+        for table_name in TABLE_LIST:
+
+            query = query_schema + """
+            select *
+            from {}
+            where patientunitstayid = {}
+            """.format(table_name, patientunitstayid)
+            cur.execute(query)
+            rows = cur.fetchall()
+            label = [desc[0] for desc in cur.description]
+
+            if len(rows) == 0:
+                json_dict[table_name] = {}
+                for idx, label_i in enumerate(label):
+                    json_dict[table_name][label_i] = []
+
+            else:
+                assert len(label) == len(rows[0])
+                json_dict[table_name] = {}
+                for idx, label_i in enumerate(label):
+                    col = [r[idx] for r in rows]
+                    col = [float(c)
+                           if isinstance(c, Decimal) else c
+                           for c in col]
+                    json_dict[table_name][label_i] = col
+
+        json_path = f"{output_folder}/{patientunitstayid}.json"
+        with open(json_path, 'w') as json_file:
+            json.dump(json_dict, json_file)
+
+    print("Total Time:", time.time()-now)
+    print('-'*80)
+
+    conn.close()
+
+'''
